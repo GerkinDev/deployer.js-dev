72e9eb271e77bf68bd71fa172cf195f7bb4f04c9
Nihilivin <alexandre.j.p.germain@gmail.com>
2016-03-08 16:49:49 -1000

    Pre-process release v0.1.24

--- deployer.js
+++ deployer.js
@@ -96,11 +96,11 @@ deployer = {
  */
 /**
  * @method process
- * @memberof deployer~action
+ * @memberof {deployer~action}
  */
 /**
  * @method test
- * @memberof deployer~action
+ * @memberof {deployer~action}
  */
 
 
