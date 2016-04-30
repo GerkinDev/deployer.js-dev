d58bbdd3fbdc6d88c4200eb963574a2695220b1b
Alexandre Germain <agermain@gerkindevelopment.net>
2016-04-30 08:34:26 -1000

    Pre-process version 0.2.3

--- actions/git.js
+++ actions/git.js
@@ -18,7 +18,7 @@ const git = require("nodegit");
  * @requires nodegit
  */
 module.exports = {
-	/**
+    /**
 	 * @method process
 	 * @static
 	 * @memberof module:actions/git
@@ -28,82 +28,82 @@ module.exports = {
 	 * @description Execute git actions with the provided config.
 	 * @tutorial git-process
      */
-	process: function(config, cb){
-		console.log("In git",config);
-		var repoPath = path.resolve(config.path ? config.path : ".");
-		git.Repository.open(repoPath).done(function(repository){
-			var signature = git.Signature.default(repository);//.create(ret.name,ret.email,(new Date()).getTime(), 0);
-			async.eachSeries(config.actions, function(action, cb1){
-				return getArgsRuntime(action, function(err, args){
-					var keysData = (action.data && action.data.constructor == Object ? Object.keys(action.data) : []);
-					var missingArgs = args.filter( function( el ) {
-						return keysData.indexOf( el ) < 0;
-					} );
-					deployer.log.info("Missing args", missingArgs);
-					async.each(missingArgs, function(arg,cb2){
-						requestPrompt("Please provide a value for \""+arg+"\" in git: ", function(val){
+    process: function(config, cb){
+        console.log("In git",config);
+        var repoPath = path.resolve(config.path ? config.path : ".");
+        git.Repository.open(repoPath).done(function(repository){
+            var signature = git.Signature.default(repository);//.create(ret.name,ret.email,(new Date()).getTime(), 0);
+            async.eachSeries(config.actions, function(action, cb1){
+                return getArgsRuntime(action, function(err, args){
+                    var keysData = (action.data && action.data.constructor == Object ? Object.keys(action.data) : []);
+                    var missingArgs = args.filter( function( el ) {
+                        return keysData.indexOf( el ) < 0;
+                    } );
+                    deployer.log.info("Missing args", missingArgs);
+                    async.each(missingArgs, function(arg,cb2){
+                        requestPrompt("Please provide a value for \""+arg+"\" in git: ", function(val){
                             action.data[arg] = val;
                             cb2();
                         });
-					}, function(){
+                    }, function(){
                         console.log(action.data)
-						switch(action.action){
-							case "commit":{
-								deployer.log.silly('GIT => Prepare commit');
-								var index;
-								var reference;
-								return repository.index().then(function(idx){
-									index = idx;
-									return index.read(1);
-								}).then(function(){
-									return index.addAll();
-								}).then(function(){
-									return index.write();
-								}).then(function(){
-									return index.writeTree();
-								}).then(function(ref){
-									reference = ref;
-									return git.Reference.nameToId(repository, "HEAD");
-								}).then(function(head){
-									return repository.getCommit(head);
-								}).then(function(parent){
-									return repository.createCommit("HEAD", signature, signature, action.data.message, reference, [parent]);
-								}).done(function(commitId){
-									deployer.log.info("GIT => Created commit " + commitId);
-									return cb1();
-								},function(err){
-									deployer.log.error('GIT => Error in "Done" while creating commit with message "' + action.data.message + '"', err);
-									return cb1();
-								});
-							} break;
+                        switch(action.action){
+                            case "commit":{
+                                deployer.log.silly('GIT => Prepare commit');
+                                var index;
+                                var reference;
+                                return repository.index().then(function(idx){
+                                    index = idx;
+                                    return index.read(1);
+                                }).then(function(){
+                                    return index.addAll();
+                                }).then(function(){
+                                    return index.write();
+                                }).then(function(){
+                                    return index.writeTree();
+                                }).then(function(ref){
+                                    reference = ref;
+                                    return git.Reference.nameToId(repository, "HEAD");
+                                }).then(function(head){
+                                    return repository.getCommit(head);
+                                }).then(function(parent){
+                                    return repository.createCommit("HEAD", signature, signature, action.data.message, reference, [parent]);
+                                }).done(function(commitId){
+                                    deployer.log.info("GIT => Created commit " + commitId);
+                                    return cb1();
+                                },function(err){
+                                    deployer.log.error('GIT => Error in "Done" while creating commit with message "' + action.data.message + '"', err);
+                                    return cb1();
+                                });
+                            } break;
 
-							case "tag":{
-								deployer.log.silly('GIT => Prepare tag');
-								return git.Reference.nameToId(repository, "HEAD").then(function(head){
-									return repository.createTag(head, action.data.label, action.data.message);
-								}).done(function(tag){
-									deployer.log.info("GIT => Created tag " + tag.name());
-									return cb1();
-								},function(error){
-									deployer.log.error('GIT => Error while tagging "' + action.data.label + '"', error);
-									return cb1();
-								});
-							} break;
+                            case "tag":{
+                                deployer.log.silly('GIT => Prepare tag');
+                                return git.Reference.nameToId(repository, "HEAD").then(function(head){
+                                    return repository.createTag(head, action.data.label, action.data.message);
+                                }).done(function(tag){
+                                    deployer.log.info("GIT => Created tag " + tag.name());
+                                    return cb1();
+                                },function(error){
+                                    deployer.log.error('GIT => Error while tagging "' + action.data.label + '"', error);
+                                    return cb1();
+                                });
+                            } break;
 
-							case "push":{
-								deployer.log.silly('GIT => Prepare pushing');
+                            case "push":{
+                                deployer.log.silly('GIT => Prepare pushing');
                                 var remote;
-								return repository.getRemote("origin", function(){
-								}).then(function(rem){
+                                return repository.getRemote("origin", function(){
+                                }).then(function(rem){
                                     remote = rem;
-									var branch = (action.data && action.data.branch) ? action.data.branch : "master";
-									if(typeof remote != "undefined" && remote != null && remote) {
-										return remote.push([
-											"refs/heads/"+branch+":refs/heads/"+branch
-										],{
-											callbacks: {
-												credentials: function(url, userName) {
-													var creds;
+                                    var branch = (action.data && action.data.branch) ? action.data.branch : "master";
+                                    if(typeof remote != "undefined" && remote != null && remote) {
+                                        return remote.push([
+                                            "refs/heads/"+branch+":refs/heads/"+branch
+                                        ],{
+                                            callbacks: {
+                                                credentials: function(url, userName) {
+                                                    var creds;
                                                     if(userName != ""){
                                                         deployer.log.silly('Using username "'+userName+'"');
                                                     } else {
@@ -117,54 +117,65 @@ module.exports = {
                                                     } else {
                                                         deployer.log.silly('Creds ok!');
                                                     }
-													return creds;
-												}
-											}
-										});
-									} else {
-										throw 'Remote "origin" not found';
-									}
-								}).catch(function(){
-                                    console.log("Failed, try login");
+                                                    return creds;
+                                                }
+                                            }
+                                        });
+                                    } else {
+                                        throw 'Remote "origin" not found';
+                                    }
+                                }).catch(function(){
+                                    console.log("Failed, try login");  
+                                    var logdata = {};
+                                    async.each(["username","password"], function(arg,cb2){
+                                        requestPrompt("Logging in - "+arg+": ", function(val){
+                                            logdata[arg] = val;
+                                            cb2();
+                                        });
+                                    }, function(){
+                                        console.log(logdata);
+                                        var creds = git.Cred.userpassPlaintextNew(logdata.username, logdata.password);
+                                        console.log(creds);
+                                    });
                                 }).done(function(){
-									deployer.log.info("GIT => Pushed to repository");
-									return cb1();
-								},function(error){
-									deployer.log.error('GIT => Error while pushing', error);
-									return cb1();
-								});
-							} break;
+                                    deployer.log.info("GIT => Pushed to repository");
+                                    return cb1();
+                                },function(error){
+                                    deployer.log.error('GIT => Error while pushing', error);
+                                    return cb1();
+                                });
+                            } break;
 
-							default: {
-								var err = "Git does not support action \"" + action.action + "\"";
-								deployer.log.error("GIT => ",err);
-								return cb1(err);
-							} break;
-						}
-						deployer.log.warn("Was not catched by switch");
-					}, function(err){
-						if(err){
-							deployer.log.error(err);
-						}
-						cb1(err);
-					});
-				});
-			}, function(err){
-				if(err)
-					deployer.log.error("GIT => ",err);
-				cb(err)
-			});
-		});
-	},
-	arguments: []
+                            default: {
+                                var err = "Git does not support action \"" + action.action + "\"";
+                                deployer.log.error("GIT => ",err);
+                                return cb1(err);
+                            } break;
+                        }
+                        deployer.log.warn("Was not catched by switch");
+                    }, function(err){
+                        if(err){
+                            deployer.log.error(err);
+                        }
+                        cb1(err);
+                    });
+                });
+            }, function(err){
+                if(err)
+                    deployer.log.error("GIT => ",err);
+                cb(err)
+            });
+        });
+    },
+    arguments: []
 }
 
 function getArgsRuntime(action, cb){
-	var args = [];
-	switch(action.action){
-		case "commit":{
-			args = ["message"];
-		} break;
-	}
-	cb(null,args);
+    var args = [];
+    switch(action.action){
+        case "commit":{
+            args = ["message"];
+        } break;
+    }
+    cb(null,args);
 }
