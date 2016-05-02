02a43df91cc4e3b4d569ded858d690cae14022cf
Alexandre Germain <agermain@gerkindevelopment.net>
2016-05-01 14:58:12 -1000

    again

--- actions/wordpress-upgrade.js
+++ actions/wordpress-upgrade.js
@@ -7,7 +7,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.en.html GPL v3
  * @package deployer.js
  *
- * @version 0.2.3
+ * @version 2.5.0
  */
 
 const randomstring = require("randomstring");
